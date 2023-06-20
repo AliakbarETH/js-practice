4b8f014fab64da30f9c93ac76967b15cf4e4cc03
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-20 13:16:51 +0500

    Comparing two Strings

--- practice.js
+++ practice.js
@@ -3,18 +3,32 @@ Strings are both treated as literals and objects
 
 */
 
-let a = "I am a literal String" ;
-let b = new String("I am an Object String") ;
+// let a = "I am a literal String" ;
+// let b = new String("I am an Object String") ;
 
-// console.log(a) ;
-// console.log(b) ;
+// // console.log(a) ;
+// // console.log(b) ;
 
-for(let i = 0 ; i < a.length ; i++){
-    console.log(a.charAt(i)) ;
+// for(let i = 0 ; i < a.length ; i++){
+//     console.log(a.charAt(i)) ;
     
+// }
+
+// for(let i = 0 ; i < a.length ; i++){
+//     console.log(b.charAt(i)) ;
+    
+// }
+
+let a = "A" ;
+let b = "AA" ;
+if(a<b){
+    console.log(`${a} is greater than ${b}`) ;
 }
 
-for(let i = 0 ; i < a.length ; i++){
-    console.log(b.charAt(i)) ;
-    
+else if(b>a) {
+    console.log(`${b} is greater than ${a}`)
+}
+
+else{
+    console.log("Both are equal") ;
 }
