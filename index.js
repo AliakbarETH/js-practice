8835c16ebe4bc73b9fd9ff54d516543a40bbf7bc
AliakbarETH <aliakbar.bcss19@iba-suk.edu.pk>
2023-06-28 23:39:17 +0500

    Used loop

--- index.js
+++ index.js
@@ -1,23 +1,3 @@
-let sec = 1 ;
-let min = 0 ;
-let hour = 0 ;
-let day = 0 ;
-
-for(let i = 0 ; i < 60 ; i++) {
-    if(sec%60 == 0) {
-        console.log(min++) ;
-
-    }
-    else if(min%60 == 0 ){
-        console.log(hour++) ;
-    }
-    else if(hour%24 == 0){
-
-        console.log(day++) ;
-     }
-
+for(let i = 0 ; i < 44 ; i++){
+    console.log(i) ;
 }
-
-
-
-
